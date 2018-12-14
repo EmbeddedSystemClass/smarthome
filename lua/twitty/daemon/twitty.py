1f9f2b65f9d3c8cfaba4c2925f43bf1ed63011c9
jesstr <jesstr@mail.ru>
2018-12-15 00:25:44 +0300

    1) Indentation; 2) VERBOSE option.

--- lua/twitty/daemon/twitty.py
+++ lua/twitty/daemon/twitty.py
@@ -6,53 +6,53 @@ from urlparse import urlparse, parse_qs
 PORT_NUMBER = 8080
 MQTT_SERVER = "localhost"
 MQTT_PORT = 1883
+VERBOSE = 1
 
 def on_connect(client, userdata, rc):
-	print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc))
 
 class myHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		print("\n----- Request from %s ----->\n" % self.address_string())
-		print(self.path)
-		request_headers = self.headers
-		content_length = request_headers.getheaders('content-length')
-		length = int(content_length[0]) if content_length else 0
-		print(request_headers)
-		print(self.rfile.read(length))
-		print("<----- Request End -----\n")
+    def print_request(self)
+        print("\n----- Request from %s ----->\n" % self.address_string())
+        print(self.path)
+        request_headers = self.headers
+        content_length = request_headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        print(request_headers)
+        print(self.rfile.read(length))
+        print("<----- Request End -----\n")
 
-		self.send_response(200)
-		self.end_headers()
+    def do_GET(self):
+        if VERBOSE:
+            print_request(self)
 
-		query_components = parse_qs(urlparse(self.path).query)
-		print("Querry:", query_components)
+        self.send_response(200)
+        self.end_headers()
 
-		try:
-				msg = ''.join(query_components["message"])
-				topic = '/' + ''.join(query_components["topic"])
-		except KeyError:
-				print "Wrong request"
-				self.wfile.write("ERROR")
-				return
-		print "Topic:", topic
-		print "Message:", msg
-		client.publish(topic, msg)
-		self.wfile.write("OK")
-		return
+        query_components = parse_qs(urlparse(self.path).query)
+        print("Querry:", query_components)
 
-	def do_POST(self):
-		print("\n----- Request from %s ----->\n" % self.address_string())
-		print(self.path)
-		request_headers = self.headers
-		content_length = request_headers.getheaders('content-length')
-		length = int(content_length[0]) if content_length else 0
-		print(request_headers)
-		print(self.rfile.read(length))
-		print("<----- Request End -----\n")
+        try:
+            msg = ''.join(query_components["message"])
+            topic = '/' + ''.join(query_components["topic"])
+        except KeyError:
+            print "Wrong request"
+            self.wfile.write("ERROR")
+            return
+            
+        print "Topic:", topic
+        print "Message:", msg
+        client.publish(topic, msg)
+        self.wfile.write("OK")
+        return
 
-		self.send_response(200)
-		self.end_headers()
-		return
+    def do_POST(self):
+        if VERBOSE:
+            print_request(self)
+
+        self.send_response(200)
+        self.end_headers()
+        return
 
 
 client = mqtt.Client()
@@ -61,10 +61,10 @@ client.on_connect = on_connect
 client.connect(MQTT_SERVER, MQTT_PORT, 60)
 
 try:
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	server.serve_forever()
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    server.serve_forever()
 
 except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+    print '^C received, shutting down the web server'
+    server.socket.close()
