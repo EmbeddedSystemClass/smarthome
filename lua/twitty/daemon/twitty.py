5483889994234c191c761668ef6cf9a49f0a42d0
jesstr <jesstr@mail.ru>
2018-12-19 17:25:52 +0300

    [twitty] Tweets parcing added

--- lua/twitty/daemon/twitty.py
+++ lua/twitty/daemon/twitty.py
@@ -2,39 +2,53 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import paho.mqtt.client as mqtt
 from urlparse import urlparse, parse_qs
+import json
 
 PORT_NUMBER = 8080
-MQTT_SERVER = "localhost"
+MQTT_SERVER = "127.0.0.1"
 MQTT_PORT = 1883
 VERBOSE = 1
 
 def on_connect(client, userdata, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
+
+def on_message(client, userdata, message):
+    print("message:", str(message.payload.decode("utf-8")))
+    print("topic:", message.topic)
 
 class myHandler(BaseHTTPRequestHandler):
-    def print_request(self)
+    def print_request(self, payload):
         print("\n----- Request from %s ----->\n" % self.address_string())
         print(self.path)
-        request_headers = self.headers
-        content_length = request_headers.getheaders('content-length')
-        length = int(content_length[0]) if content_length else 0
-        print(request_headers)
-        print(self.rfile.read(length))
+        print(self.headers)
+        if payload:
+            print(payload)
         print("<----- Request End -----\n")
 
     def do_GET(self):
-        if VERBOSE:
-            print_request(self)
-
         self.send_response(200)
         self.end_headers()
 
         query_components = parse_qs(urlparse(self.path).query)
-        print("Querry:", query_components)
+
+        content_length = self.headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        
+        tweet = {}
+        payload = None
+        if length:
+            payload = self.rfile.read(length)
+            if payload:
+                tweet = json.loads(payload)
+        
+        if VERBOSE:
+            self.print_request(payload)
 
         try:
-            msg = ''.join(query_components["message"])
             topic = '/' + ''.join(query_components["topic"])
+            text = tweet["text"]
+            user = tweet["username"]
+            msg = user + ':' + text
         except KeyError:
             print "Wrong request"
             self.wfile.write("ERROR")
@@ -47,24 +61,63 @@ class myHandler(BaseHTTPRequestHandler):
         return
 
     def do_POST(self):
-        if VERBOSE:
-            print_request(self)
-
         self.send_response(200)
         self.end_headers()
+
+        query_components = parse_qs(urlparse(self.path).query)
+        
+        content_length = self.headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        
+        tweet = {}
+        payload = None
+        if length:
+            payload = self.rfile.read(length)
+            print payload
+            if payload:
+                try:
+                    tweet = json.loads(payload)
+                except ValueError:
+                    print "Wrong payload: " + payload
+                    return
+
+        
+        if VERBOSE:
+            self.print_request(payload)
+
+        try:
+            topic = '/' + ''.join(query_components["topic"])
+            text = tweet["text"]
+            user = tweet["username"]
+            msg = user + ':' + text
+        except KeyError:
+            print "Wrong request"
+            #self.wfile.write("ERROR")
+            return
+            
+        print "Topic:", topic
+        print "Message:", msg.encode("utf-8")
+
+        client.publish(topic, msg)
+        self.wfile.write("OK")
         return
 
 
 client = mqtt.Client()
 client.on_connect = on_connect
+client.on_message = on_message
 
 client.connect(MQTT_SERVER, MQTT_PORT, 60)
+client.loop_start()
+#client.subscribe("/stats", qos=0)
+
 
 try:
     server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print 'Started httpserver on port ' , PORT_NUMBER
+    print 'Started httpserver on port', PORT_NUMBER
     server.serve_forever()
 
 except KeyboardInterrupt:
     print '^C received, shutting down the web server'
     server.socket.close()
+    client.loop_stop()
